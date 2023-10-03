f008c4b262120a7b5367d4d8d72d6e6de0ae307a
TANHEHANZ <hantach10@gmail.com0>
2023-10-03 01:21:46 -0400

    put delete controllers

--- BackendDataBasePrisma/src/routes/task.routes.js
+++ BackendDataBasePrisma/src/routes/task.routes.js
@@ -16,4 +16,28 @@ router.post("/task", async (req, res) => {
   res.json(newtarea);
 });
 
+router.put("/task/:id", async (req, res) => {
+  const taskId = parseInt(req.params.id);
+
+  try {
+    const updatedTask = await prisma.Task.update({
+      where: { id: taskId },
+      data: req.body,
+    });
+
+    res.json(updatedTask);
+  } catch (error) {
+    console.error("Error al actualizar la tarea:", error);
+    res.status(500).json({ error: "Error al actualizar la tarea" });
+  }
+});
+
+router.delete("/task/:id", async (req, res) => {
+  const deleteTarea = await prisma.Task.delete({
+    where: { id: parseInt(req.params.id) },
+  });
+  if (!deleteTarea) return res.status(404).json({ error: "Task not found" });
+  return res.json({ deleteTarea });
+});
+
 export default router;
